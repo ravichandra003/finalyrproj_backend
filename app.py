1cc20a300c402dba51d608470f13e2150418d861
ravichandra003 <121211922+ravichandra003@users.noreply.github.com>
2024-12-29 19:29:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask, request, jsonify
 import subprocess
 import os
 import tempfile
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def index():
