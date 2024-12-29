b2369b85eae053942c212510120a98504c910ede
ravichandra003 <121211922+ravichandra003@users.noreply.github.com>
2024-12-29 19:55:16 +0530

    Update app.py test

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def upload_file():
     
     try:
         # Run the Python scripts or perform other processing
-        result1 = subprocess.run(['python3', 'det1.py', file_path], capture_output=True, text=True)
+        result1 = "result1"#subprocess.run(['python3', 'det1.py', file_path], capture_output=True, text=True)
         result2 = "result2" #subprocess.run(['python3', 'det2.py', file_path], capture_output=True, text=True)
         result3 = "result3" #subprocess.run(['./yara-master/yara', 'test.yara', file_path], capture_output=True, text=True)
         
