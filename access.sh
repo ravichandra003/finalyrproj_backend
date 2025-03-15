5556e835ede810d65e955871ff8420fb3dc98409
ravichandra003 <121211922+ravichandra003@users.noreply.github.com>
2025-03-16 02:11:37 +0530

    Update access.sh

--- access.sh
+++ access.sh
@@ -3,19 +3,6 @@
 
 
 
-
-# Manually add missing auxiliary files
-echo "Downloading missing auxiliary files..."
-wget https://git.savannah.gnu.org/cgit/config.git/plain/config.guess
-wget https://git.savannah.gnu.org/cgit/config.git/plain/config.sub
-wget https://git.savannah.gnu.org/cgit/config.git/plain/compile
-wget https://git.savannah.gnu.org/cgit/config.git/plain/ar-lib
-wget https://git.savannah.gnu.org/cgit/config.git/plain/missing
-wget https://git.savannah.gnu.org/cgit/config.git/plain/install-sh
-mkdir -p build-aux
-mv config.guess config.sub compile ar-lib missing install-sh build-aux/
-echo "Missing auxiliary files added successfully."
-
 # Configure the build system
 echo "Configuring the build system..."
 ./bootstrap.sh
